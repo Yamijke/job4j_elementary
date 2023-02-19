3ef04bc9d89077ccadde75365cb1c84f34976b1a
Yamijke <serhei.knyazev@gmail.com>
2023-02-19 17:49:40 +0300

    Тесты для определения расстояния между точками. [#159459 #392670]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -17,7 +17,7 @@ public class PointTest {
     }
 
     @Test
-    public void when20to30then2() {
+    public void when10to30then2() {
         double expected = 2;
         int x1 = 1;
         int y1 = 0;
@@ -28,7 +28,7 @@ public class PointTest {
     }
 
     @Test
-    public void whenMin20to30then5() {
+    public void whenMinus20to30then5() {
         double expected = 5;
         int x1 = -2;
         int y1 = 0;
@@ -39,7 +39,7 @@ public class PointTest {
     }
 
     @Test
-    public void whenMin30to30then6() {
+    public void whenMinus30to30then6() {
         double expected = 6;
         int x1 = -3;
         int y1 = 0;
