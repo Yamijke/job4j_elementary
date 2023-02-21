c44ea0bdd57407a75f50eccf352267a5d0896066
Yamijke <serhei.knyazev@gmail.com>
2023-02-21 16:58:49 +0300

    Множественное логическое выражение И [#9461 #393594]

--- src/test/java/ru/job4j/condition/TriangleTest.java
+++ src/test/java/ru/job4j/condition/TriangleTest.java
@@ -1,10 +1,10 @@
 package ru.job4j.condition;
 
 import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TriangleTest {
+
     @Test
     public void whenExist() {
         double ab = 2.0;
