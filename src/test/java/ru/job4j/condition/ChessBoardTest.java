9948c444167aa2635bef3dff0e5a12d79d28efc7
Yamijke <serhei.knyazev@gmail.com>
2023-02-21 21:16:16 +0300

    Math.abs [#373616 #393653]

--- src/test/java/ru/job4j/condition/ChessBoardTest.java
+++ src/test/java/ru/job4j/condition/ChessBoardTest.java
@@ -7,35 +7,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ChessBoardTest {
 
     @Test
-    public void wayIs5() {
+    public void wayIs10() {
         int x1 = 6;
         int y1 = 7;
         int x2 = 1;
         int y2 = 2;
         int result = ChessBoard.way(x1, y1, x2, y2);
-        int expected = 5;
+        int expected = 10;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    public void wayIs7() {
+    public void wayIs14() {
         int x1 = 7;
         int y1 = 0;
         int x2 = 0;
         int y2 = 7;
         int result = ChessBoard.way(x1, y1, x2, y2);
-        int expected = 7;
+        int expected = 14;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    public void wayIs0() {
+    public void wayIs7() {
         int x1 = 2;
         int y1 = 6;
         int x2 = 4;
         int y2 = 1;
         int result = ChessBoard.way(x1, y1, x2, y2);
-        int expected = 0;
+        int expected = 7;
         assertThat(result).isEqualTo(expected);
     }
 
