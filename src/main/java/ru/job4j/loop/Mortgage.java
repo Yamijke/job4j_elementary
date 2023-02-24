478a335ebd5a8e23788549749d45bcd0d9a23585
Yamijke <serhei.knyazev@gmail.com>
2023-02-24 15:42:24 +0300

    Ипотека [#156315 #394630]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -2,12 +2,9 @@ package ru.job4j.loop;
 
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
-        if (salary >= amount + amount * (percent / 100)) {
-            return 1;
-        }
         int year = 0;
-        while (amount >= 0) {
-            amount = amount + amount * (percent / 100) - salary;
+        while (amount > 0) {
+            amount += amount * (percent / 100) - salary;
             year++;
         }
         return year;
