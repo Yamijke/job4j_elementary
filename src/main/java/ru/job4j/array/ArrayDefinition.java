dc343e9850bafcf8f041666566c4db9fa83c2b25
Yamijke <serhei.knyazev@gmail.com>
2023-02-25 10:59:49 +0300

    Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,16 @@ public class ArrayDefinition {
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+        String[] names = new String[5];
+        names[0] = "Vladimir";
+        names[1] = "Marina";
+        names[2] = "Sergey";
+        names[3] = "Maria";
+        names[4] = "Anna";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+        System.out.println(names[4]);
     }
 }
