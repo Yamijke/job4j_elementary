86458b0ee053eb8495fa5e81b495f64af5e37af5
Yamijke <serhei.knyazev@gmail.com>
2023-02-26 12:57:40 +0300

    Классический поиск перебором. [#33489]

---
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class FindLoop {
+    public static int indexOf(int[] data, int el) {
+        int rst = -1;
+        for (int index = 0; index < data.length; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+
+    public static void main(String[] args) {
+        int[] data = {1, 2, 3, 4, 5};
+        System.out.println(FindLoop.indexOf(data, 3));
+    }
+}
