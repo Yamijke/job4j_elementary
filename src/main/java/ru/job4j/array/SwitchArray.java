e95229be76d32a7ca52f24d50004421d82b5d26d
Yamijke <serhei.knyazev@gmail.com>
2023-02-26 14:55:47 +0300

    Переставить элементы массива [#226795]

--- src/main/java/ru/job4j/array/SwitchArray.java
+++ src/main/java/ru/job4j/array/SwitchArray.java
@@ -1,6 +1,14 @@
 package ru.job4j.array;
 
 public class SwitchArray {
+    public static int[] swap(int[] array, int source, int dest) {
+        int index = array[source];
+        int index2 = array[dest];
+        array[dest] = index;
+        array[source] = index2;
+        return array;
+    }
+
     public static int[] swapBorder(int[] array) {
         int temp = array[0];
         array[0] = array[array.length - 1];
