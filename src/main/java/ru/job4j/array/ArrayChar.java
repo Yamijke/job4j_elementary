5760f5d9b67d72120c73ea3018c8b2c4ee4f9bb6
Yamijke <serhei.knyazev@gmail.com>
2023-02-27 18:17:36 +0300

    Массив заполнен true или false [#53857]

---
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+        for (int i = 0; i <= pref.length - 1; i++) {
+            if (word[i] != pref[i]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] pref = {'H', 'i'};
+        boolean result = ArrayChar.startsWith(word, pref);
+        System.out.println(result);
+    }
+}
