2861214d02a3a8f4748b337a52054413aabb559c
Yamijke <serhei.knyazev@gmail.com>
2023-03-05 14:28:21 +0300

    String - это массив [#504884]

---
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -0,0 +1,37 @@
+package ru.job4j.array;
+
+public class JavaNameValidator {
+    public static boolean isNameValid(String name) {
+        if (name == null || name.isEmpty()) {
+            return false;
+        }
+
+        if (!Character.isLowerCase(name.codePointAt(0))) {
+            return false;
+        }
+
+        for (int i = 0; i < name.length(); i++) {
+            int code = name.codePointAt(i);
+            if (!isSpecialSymbol(code) && !isUpperLatinLetter(code) && !isLowerLatinLetter(code) && !isDigit(code)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isDigit(int code) {
+        return code >= 48 && code <= 57;
+    }
+
+    public static boolean isSpecialSymbol(int code) {
+        return code == 36 || code == 95;
+    }
+
+    public static boolean isUpperLatinLetter(int code) {
+        return code >= 65 && code <= 90;
+    }
+
+    public static boolean isLowerLatinLetter(int code) {
+        return code >= 97 && code <= 122;
+    }
+}
